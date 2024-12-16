5e24464f3bea16178790c4fe1bc7b520a0834a9f
erika.duarte <erika.duarte.mora@gmail.com>
2024-12-15 22:31:41 -0500

    feat(search) : subtracted method that iterated through the room list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -296,6 +296,7 @@ public class Main {
         return adjustment;
     }
 
+
     public static List<Object[]> searchAvailableAccommodations(
             String city,
             String typeOfAccommodation,
